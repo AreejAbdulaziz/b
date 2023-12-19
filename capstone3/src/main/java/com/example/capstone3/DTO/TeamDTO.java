f7700a38e376549ff6ab3713804178adeec4d508
AreejAbdulaziz <148686069+AreejAbdulaziz@users.noreply.github.com>
2023-12-19 15:09:43 +0300

    Add files via upload

---
+++ capstone3/src/main/java/com/example/capstone3/DTO/TeamDTO.java
@@ -0,0 +1,11 @@
+package com.example.capstone3.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TeamDTO {
+    private Integer hackathon_id;
+    private Integer member_id;
+}
